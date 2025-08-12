ae89610517dfe026e2fd2043c96b87fc6809757b
AlishaDarly-mountbox <alisha.d@mountbox.in>
2025-08-12 11:06:53 +0530

    login edits3

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,2 +1,14 @@
-// Auth disabled: allow all requests through
-module.exports = (_req, _res, next) => next();
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.userData = { userId: decodedToken.userId };
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Authentication failed'
+        });
+    }
+};
